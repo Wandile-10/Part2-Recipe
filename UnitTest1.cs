9670add2ee4a7debd9c985dedb9ff8371cc41e39
Wandile Phakathi <129935738+Wandile-10@users.noreply.github.com>
2024-05-13 14:43:17 +0000

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RecipeApplicationTests
+{
+    [TestFixture]
+    public class RecipeTests
+    {
+        [Test]
+        public void AddIngredient_WithValidInput_ShouldAddIngredient()
+        {
+            // Arrange
+
+            // Act
+            ((Recipe)new Recipe("Test Recipe")).AddIngredient("Test Ingredient", 100, "grams", 50, "Test Food Group");
+
+            // Assert
+            Assert.AreEqual(1, ((Recipe)new Recipe("Test Recipe")).Ingredients.Count);
+        }
+
+        [Test]
+        public void Scale_WithValidFactor_ShouldScaleIngredients()
+        {
+            // Arrange
+            Recipe recipe = new Recipe("Test Recipe");
+            recipe.AddIngredient("Test Ingredient 1", 100, "grams", 50, "Test Food Group");
+            recipe.AddIngredient("Test Ingredient 2", 200, "grams", 100, "Test Food Group");
+
+            // Act
+            recipe.Scale(2);
+
+            // Assert
+            foreach (var ingredient in recipe.Ingredients)
+            {
+                Assert.AreEqual(200, ingredient.Quantity);
+                Assert.AreEqual(100, ingredient.Calories);
+            }
+        }
+    }
+
+    internal class Recipe
+    {
+        internal object Ingredients;
+
+        public Recipe(string v)
+        {
+            V = v;
+        }
+
+        public string V { get; }
+
+        internal void AddIngredient(string v1, int v2, string v3, int v4, string v5)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Scale(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class TestAttribute : Attribute
+    {
+    }
+
+    internal class TestFixtureAttribute : Attribute
+    {
+    }
+
+    [TestFixture]
+    public class RecipeManagerTests
+    {
+        [Test]
+        public void EnterRecipeDetails_WithValidInput_ShouldAddRecipe()
+        {
+            // Arrange
+            RecipeManager recipeManager = new RecipeManager();
+            int initialCount = recipeManager.Recipes.Count;
+
+            // Act
+            recipeManager.EnterRecipeDetails();
+
+            // Assert
+            Assert.AreEqual(initialCount + 1, recipeManager.Recipes.Count);
+        }
+    }
+
+    internal class RecipeManager
+    {
+        public object Recipes { get; internal set; }
+
+        internal void EnterRecipeDetails()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
